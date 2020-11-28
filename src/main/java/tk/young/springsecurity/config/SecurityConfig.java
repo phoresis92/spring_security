19cd11a9d1c824aac4e354617286b0900023abba
kyd <husheart@naver.com>
2020-11-28 18:01:21 +0900

    5) Logout 처리, LogoutFilter
    스프링 시큐리티 기본 API 및 Filter 이해

--- src/main/java/tk/young/springsecurity/config/SecurityConfig.java
+++ src/main/java/tk/young/springsecurity/config/SecurityConfig.java
@@ -7,10 +7,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @EnableWebSecurity
@@ -42,6 +45,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .anyRequest().authenticated();
+
+        http
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .addLogoutHandler((request, response, authentication) -> {
+                    HttpSession session = request.getSession();
+                    session.invalidate();
+                })
+                .logoutSuccessHandler((request, response, authentication) -> {
+                    response.sendRedirect("/login");
+                })
+                .deleteCookies("JSESSIONID");
+//                .deleteCookies("remember-me");
 
     }
 
