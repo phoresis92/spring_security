c662adb6e4ad2fb5a4acb5b41027ff46b5a905e1
kyd <husheart@naver.com>
2020-11-29 02:49:38 +0900

    11) 권한설정과 표현식
    스프링 시큐리티 기본 API 및 Filter 이해

--- src/main/java/tk/young/springsecurity/config/SecurityConfig.java
+++ src/main/java/tk/young/springsecurity/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package tk.young.springsecurity.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -28,6 +29,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserDetailsService userDetailsService;
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//      스프링 5부터 암호앞에 prefix 로 암호화 방식을 명시해줘야한다. {noop} 평문 저장
+        auth.inMemoryAuthentication().withUser("user").password("{noop}123").roles("USER");
+        auth.inMemoryAuthentication().withUser("sys").password("{noop}123").roles("SYS", "USER");
+        auth.inMemoryAuthentication().withUser("admin").password("{noop}123").roles("ADMIN", "SYS", "USER");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         // Authentication
@@ -38,21 +47,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .failureUrl("/login")
                 .usernameParameter("userId")
                 .passwordParameter("passwd")
-                .loginProcessingUrl("/login_proc")
+//                .loginProcessingUrl("/login_proc")
                 .successHandler((request, response, authentication) -> {
                     System.out.println("authentication.getName() = " + authentication.getName());
                     response.sendRedirect("/");
                 })
-//                .failureHandler((request, response, exception) -> {
-//                    System.out.println("exception.getMessage() = " + exception.getMessage());
-//                    response.sendRedirect("/");
-//                })
+                .failureHandler((request, response, exception) -> {
+                    exception.printStackTrace();
+                    System.out.println("exception.getMessage() = " + exception.getMessage());
+                    response.sendRedirect("/");
+                })
                 .permitAll();
 
         // Authorization
-        http
-                .authorizeRequests()
-                .anyRequest().authenticated();
+//        http
+//                .authorizeRequests()
+//                .anyRequest().authenticated();
 
         // Logout
         http
@@ -73,7 +83,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .rememberMe()
                 .rememberMeParameter("remember") // 기본 파라미터명은 remember-me
                 .tokenValiditySeconds(3600) // Default 14일
-                .alwaysRemember(true) // 리멤버 미 기능이 활성화되지 않아도 항상 실행
+//                .alwaysRemember(true) // 리멤버 미 기능이 활성화되지 않아도 항상 실행
                 .userDetailsService(userDetailsService);
 
         // Session Management
@@ -110,6 +120,37 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         NEVER : 생성하지 않지만 이미 존재하면 사용
         STATELESS : 생성하지 않고 존재해도 사용하지 않음 (JWT 사용시 해당 설정 사용할것)
 */
+
+        // 권한 설정
+        // 주의사항 - 설정 시 구체적인 경로가 먼저 오고 그것 보다 큰 범위의 경로가 뒤에 오도록 해야 한다.
+        http
+                .authorizeRequests()
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/admin/pay").hasRole("ADMIN")
+                .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
+                .anyRequest().authenticated();
+
+//                .antMatcher("/shop/**").authorizeRequests()
+//                .antMatchers("/shop/login", "/shop/users/**").permitAll()
+//                .antMatchers("/shop/mypage").hasRole("USER")
+//                .antMatchers("/shop/admin/pay").access("hasRole('ADMIN')")
+//                .antMatchers("/shop/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
+//                .anyRequest().authenticated();
+
+        /*
+            authenticated()                 인증된 사용자의 접근을 허용
+            fullyAuthenticated()            인증된 사용자의 접근을 허용, rememberMe 인증 제
+            permitAll()                     무조건 접근을 허용
+            denyAll()                       무조건 접근을 허용하지 않음
+            anonymous()                     익명사용자의 접근을 허용 (Role User는 접근 불)
+            rememberMe()                    기억하기를 통해 인증된 사용자의 접근을 허용
+            access(String)                  주어진 SpEL 표현식의 평가 결과가 true이면 접근을 허용
+            hasRole(String)                 사용자가 주어진 역할이 있다면 접근을 허용가 (Role prefix를 붙이지 않는다)
+            hasAuthority(String)            사용자가 주어진 권한이 있다면 (Role prefix를 붙여야 한다)
+            hasAnyRole(String...)           사용자가 주어진 권한이 있다면 접근을 허용
+            hasAnyAuthority(String...)      사용자가 주어진 권한 중 어떤 것이라도 있다면 접근을 허용
+            hasIpAddress(String)            주어진 IP로부터 요청이 왔다면 접근을 허용
+        */
 
     }
 
